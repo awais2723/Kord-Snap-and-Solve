62808dbbb754117f48892fdc2dd5dcf10ac93be2
AreebGhani <areebghanidps@gmail.com>
2024-04-14 02:50:41 +0500

    feat: Document Scanning
    Scan documents using react-native-document-scanner-plugin
    
    fix #5

--- src/screens/ScanScreen.tsx
+++ src/screens/ScanScreen.tsx
@@ -1,12 +1,62 @@
-import { View } from 'react-native';
+/* This code snippet is a React component called `ScanScreen` that utilizes the `useState` and
+`useEffect` hooks from React. It also imports necessary components and modules such as `Platform`,
+`PermissionsAndroid`, `Image`, `Alert`, `View`, and `DocumentScanner` for handling document scanning
+functionality. */
+import React, { useState, useEffect } from 'react';
+import { Platform, PermissionsAndroid, Image, Alert, View } from 'react-native';
+import DocumentScanner from 'react-native-document-scanner-plugin';
 
 import { Card } from '@/src/components';
 
-const ScanScreen = () => (
-  <View className="bg-gray-900 flex flex-col flex-1 justify-center items-center">
-    <Card text="Scan Screen" />
-    <View className="my-10" />
-  </View>
-);
+const ScanScreen = () => {
+  const [scannedImage, setScannedImage] = useState<string>('');
+
+  const scanDocument = async () => {
+    // prompt user to accept camera permission request if they haven't already
+    if (
+      Platform.OS === 'android' &&
+      (await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA)) !==
+        PermissionsAndroid.RESULTS.GRANTED
+    ) {
+      Alert.alert('Error', 'User must grant camera permissions for scanning.');
+      return;
+    }
+
+    // start the document scanner
+    const { scannedImages } = await DocumentScanner.scanDocument({
+      maxNumDocuments: 1,
+    });
+
+    // get back an array with scanned image file paths
+    if (scannedImages && scannedImages.length > 0) {
+      // set the img src, so we can view the first scanned image
+      setScannedImage(scannedImages[0]);
+    }
+  };
+
+  useEffect(() => {
+    // call scanDocument on load
+    scanDocument();
+  }, []);
+
+  if (scannedImage) {
+    return (
+      <View className="bg-gray-900 flex flex-col flex-1 justify-center items-center">
+        <Image
+          resizeMode="contain"
+          source={{ uri: scannedImage }}
+          className="w-full h-full -mt-12"
+        />
+      </View>
+    );
+  }
+
+  return (
+    <View className="bg-gray-900 flex flex-col flex-1 justify-center items-center">
+      <Card text="Scan Screen" />
+      <View className="my-10" />
+    </View>
+  );
+};
 
 export default ScanScreen;
