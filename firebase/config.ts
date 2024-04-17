fa9626dcabee25264a3b66be7b480e93197ad686
AreebGhani <areebghanidps@gmail.com>
2024-04-17 21:08:52 +0500

    feat: Firebase Setup
    Connecting app with the Firebase
    
    fix #2

---
+++ firebase/config.ts
@@ -0,0 +1,16 @@
+/* Setting up a Firebase app using the Firebase SDK. Here's a breakdown of what
+each part is doing: */
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
